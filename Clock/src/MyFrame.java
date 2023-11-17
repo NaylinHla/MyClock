61f5c12624c08466c5f5d93317cd7b3092e4c0a3
Naylin <138529784+NaylinHla@users.noreply.github.com>
2023-11-17 12:01:50 +0100

    Update MyFrame.java

--- Clock/src/MyFrame.java
+++ Clock/src/MyFrame.java
@@ -4,9 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class MyFrame extends JFrame {
-
-
-    Calendar calendar;
+    
     SimpleDateFormat timeFormat;
     SimpleDateFormat dayFormat;
     SimpleDateFormat dateFormat;
